512d6a403569aabcc7a9d56d2e5188b5e8f41ae9
Pankaj Pande <pankaj.pande@elektrobit.com>
2025-09-03 22:09:15 +0530

    Commit updated QNX Audio wapper development remaining changes

---
+++ pjlib/include/pj/config_site.h
@@ -0,0 +1,21 @@
+#ifndef __PJ_CONFIG_SITE_H__
+#define __PJ_CONFIG_SITE_H__
+
+#define PJ_HAS_CONFIG_SITE 1
+#define PJMEDIA_AUDIO_DEV_HAS_QNX 1 // QNX specific audio device support
+#define PJ_QNX 1
+#define PJ_OS_QNX 1
+#define PJ_CONFIG_QNX 1
+#define PJ_HAS_STDDEF_H 1
+#define PJ_HAS_HIGH_RES_TIMER 1
+#define PJ_EMULATE_RWMUTEX 0
+#define PJ_THREAD_SET_STACK_SIZE 1
+#define PJ_THREAD_ALLOCATE_STACK 1
+#define PJ_HAS_UNISTD_H 1
+#define PJ_HAS_SYS_TYPES_H 1
+#define PJ_HAS_SYS_SOCKET_H 1
+#define PJ_HAS_NETINET_IN_H 1
+#define PJ_HAS_ARPA_INET_H 1
+#define PJ_HAS_IFADDRS_H 1
+
+#endif // __PJ_CONFIG_SITE_H__
